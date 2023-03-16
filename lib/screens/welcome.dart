5290dda8eced31dd42f9722eeb531b222762d4cd
Harshavardhan <harshabamane1718@gmail.com>
2023-03-16 12:00:25 +0530

    added images

---
+++ lib/screens/welcome.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage('images/welcome.jpg'),
+      ),
+    ));
+  }
+}
