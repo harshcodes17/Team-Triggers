11c1f06e5a5e0c7ebe781d69e297b935f493127e
Harshavardhan <harshabamane1718@gmail.com>
2023-03-16 13:08:09 +0530

    added new font

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -27,11 +27,11 @@ class _WelcomePageState extends State<WelcomePage> {
                 width: 700,
                 child: Padding(
                   padding:
-                      const EdgeInsets.only(left: 40.0, top: 120, right: 12.0),
+                      const EdgeInsets.only(left: 30.0, top: 180, right: 12.0),
                   child: Text(
                     'Welcome Student!',
                     style: TextStyle(
-                      fontSize: 36,
+                      fontSize: 38,
                       color: Colors.blue.shade300,
                       fontFamily: 'Pacifico',
                       fontWeight: FontWeight.bold,
@@ -40,7 +40,65 @@ class _WelcomePageState extends State<WelcomePage> {
                 ),
               ),
             ),
-            Row()
+            // SingleChildScrollView(
+            //   child: Container(
+            //     padding: EdgeInsets.only(
+            //       left: 25,
+            //       top: MediaQuery.of(context).size.height * 0.5,
+            //       right: 25,
+            //     ),
+            //     child: Column(
+            //       children: [
+            //         TextField(
+            //           decoration: InputDecoration(
+            //             fillColor: Colors.blue.shade100,
+            //             filled: true,
+            //             hintText: 'Email',
+            //             border: OutlineInputBorder(
+            //               borderRadius: BorderRadius.circular(10),
+            //             ),
+            //           ),
+            //         ),
+            //         SizedBox(
+            //           height: 20,
+            //         ),
+            //         TextField(
+            //           decoration: InputDecoration(
+            //             fillColor: Colors.blue.shade100,
+            //             filled: true,
+            //             hintText: 'Password',
+            //             border: OutlineInputBorder(
+            //               borderRadius: BorderRadius.circular(10),
+            //             ),
+            //           ),
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // )
+            Padding(
+              padding: const EdgeInsets.only(left: 98.0, top: 400),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 90,
+                    height: 35,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Log in',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue.shade300,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
