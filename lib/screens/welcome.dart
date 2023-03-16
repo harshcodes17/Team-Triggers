bc05151b3653344c481ac69090bf8dd7f67fd4f8
Harshavardhan <harshabamane1718@gmail.com>
2023-03-16 12:34:48 +0530

    added new images

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -11,10 +11,39 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-      image: DecorationImage(
-        image: AssetImage('images/welcome.jpg'),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              'images/—Pngtree—hand drawn flat cartoon background_1068944.jpg'),
+          fit: BoxFit.cover,
+        ),
       ),
-    ));
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Container(
+                width: 700,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 40.0, top: 120, right: 12.0),
+                  child: Text(
+                    'Welcome Student!',
+                    style: TextStyle(
+                      fontSize: 36,
+                      color: Colors.blue.shade300,
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Row()
+          ],
+        ),
+      ),
+    );
   }
 }
