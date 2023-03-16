309870997ee4f209a4ebf9789ff42ffa934ab852
Vishal <desaivishal4904@gmail.com>
2023-03-16 15:29:23 +0530

    added login

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:student_facilitation/screens/welcome.dart';
 import 'package:student_facilitation/screens/login.dart';
-import 'package:student_facilitation/screens/register.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'welcome',
-    routes: {
-      'welcome': (context) => WelcomePage(),
-      'login': (context) => MyLogin(),
-      'register': (context) => RegisterPage(),
-    },
-  ));
+  runApp(const MyLogin());
 }
